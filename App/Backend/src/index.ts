1be0031b8663fcfd536b55d4ba84ddf36685a82c
farhadjaman <jamanfarhad1@gmail.com>
2025-02-10 00:34:47 +0600

    feat: initialize backend with postgres docker

---
+++ App/Backend/src/index.ts
@@ -0,0 +1,48 @@
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import express from "express";
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  handler: (_req, res) => {
+    res.status(429).json({
+      message: "Too many requests, please try again later.",
+    });
+  },
+});
+app.use("/api", limiter);
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+//health check
+app.get("/health", (_req, res) => {
+  res.json({ message: "API Gateway is up and running" });
+});
+
+// 404 handler
+app.use((_req, res) => {
+  res.status(404).json({ message: "Not Found" });
+});
+
+// Error handler
+app.use((err, _req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`API Gateway is running on port ${port}`);
+});
