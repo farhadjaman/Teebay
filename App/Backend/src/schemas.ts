b6dbd2b506ffbccf45be6f3c2cd857f47fb91833
farhadjaman <jamanfarhad1@gmail.com>
2025-02-10 00:35:52 +0600

    feat: setup prisma

---
+++ App/Backend/src/schemas.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const UserCreateSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(100),
+  name: z.string().min(3).max(100),
+});
+
+export const UserLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+export const AccessTokenSchema = z.object({
+  accessToken: z.string(),
+});
